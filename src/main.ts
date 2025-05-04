ee0197a3c1ce7055449cf67ea77010ad90212e8d
petikosa <petikosa@yahoo.com>
2025-05-04 19:50:08 -0400

    Improved front end

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {corsInterceptor} from './app/interceptors/cors.interceptor';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {providers: [
+    provideHttpClient(withInterceptors([corsInterceptor])),
+  ]}).catch(err => console.error(err));
